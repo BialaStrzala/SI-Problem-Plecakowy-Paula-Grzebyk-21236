80838af3739a38fa66f4acc8f44a0dd0552b425a
BIALASTRZALA2\paula <paulagrzebyk@gmail.com>
2026-03-14 16:36:26 +0100

    -

--- alg_plecakowy.py
+++ alg_plecakowy.py
@@ -15,8 +15,8 @@ HMCR = 0.7
 # Liczba iteracji
 NI = 100
 # Dane zadania
-limit_wagi = 300        #[kg]
-limit_liczby = 100      #[paczki]
+limit_wagi = 100*1000           #[kg->g]
+limit_liczby = 10               #[paczki]
 
 # Losowo wybiera pierwotny zbior paczek
 def init_random_hms(csv_file):
@@ -24,29 +24,40 @@ def init_random_hms(csv_file):
     HM = []
     paczki = []
 
-    with open(csv_file, mode='r') as file:
-        reader = csv.reader(file, delimiter=';')
-        next(reader) # naglowek
+    with open(csv_file, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
         for row in reader:
-            waga = float(row[1])
-            priorytet = float(row[2])
+            waga = float(row['weight'])
+            priorytet = float(row['priority'])
             paczki.append((waga, priorytet))
+            #print(f"{waga}, {priorytet}")
 
+    
+    # n-zestawow (HMS)
     for _ in range(HMS):
         zestaw = []
         suma_wagi = 0
-
         while len(zestaw) < limit_liczby and suma_wagi < limit_wagi:
             paczka = random.choice(paczki)
+            #print('wybrana paczka (waga): ' + f"{paczka[0]}")
             if suma_wagi + paczka[0] <= limit_wagi:
                 zestaw.append(paczka)
                 suma_wagi += paczka[0]
+                #print('dodaje paczke do zestawu')
+            else:
+                #print('odrzucam paczke')
+                break
         HM.append(zestaw)
         #HMwartosc.append(fitness(zestaw))
+        print('=== Kolejne startowe zestawy ===')
+        print('Liczba paczek = ' + f"{len(zestaw)}" + ', suma_wagi = ' + f"{suma_wagi:.2f}")
+
 
 # Funkcja przystosowania
-#def fitness():
-    #...
+def fitness(HM):
+    suma_priorytetu = [sum(paczka[1] for paczka in sublist) for sublist in HM]
+    print(suma_priorytetu)
+    
 
 #def punish():
     #...
@@ -54,3 +65,9 @@ def init_random_hms(csv_file):
 def main():
     init_random_hms('Paczki_dane.csv')
     print(HM)
+    print('=== HM ===')
+    print('Liczba zestawow = ' + f"{len(HM)}")
+    print('Dziala')
+    fitness(HM)
+
+main()
